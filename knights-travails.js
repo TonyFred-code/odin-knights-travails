ab0ec273b79e23677a085ca2faac661fd064ad76
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2024-01-31 05:21:12 +0100

    add buildBoard method to chessBoard class
    create chessBoard class

--- knights-travails.js
+++ knights-travails.js
@@ -4,3 +4,30 @@ class Node {
         this.possibleMoves = [];
     }
 }
+
+class ChessBoard {
+    constructor() {
+        this.vertices = [];
+        this.#buildBoard();
+    }
+
+    #buildBoard() {
+        for (let y = 0; y < 8; y++) {
+            for (let x = 0; x < 8; x++) {
+                const node = new Node(x, y);
+                this.#addPossibleMoves(node);
+                this.vertices.push(node);
+            }
+        }
+    }
+
+    #addPossibleMoves(node) {
+        const address = node.address;
+        const [x, y] = address;
+        const upperLeft = [x-1, y + 2]
+        console.log(upperLeft);
+    }
+}
+
+const board = new ChessBoard();
+console.log(board.vertices);
